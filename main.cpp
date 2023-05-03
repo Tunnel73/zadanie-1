2d132b333311fa13cbd62a8f7aaf91ee2599a120
Tunnel73 <131802557+Tunnel73@users.noreply.github.com>
2023-05-03 19:50:50 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,3 +16,34 @@ int main(){
 }
 return 0;
 }
+
+
+
+# Номер - 2.
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+int a[] = {1, 4, 6};
+int b[] = {11, 33, 22};
+int n = sizeof(a) / sizeof(a[0]);
+bool swapped;
+do {
+swapped = false;
+for (int i = 0; i < n - 1; i++) {
+if (b[i] > b[i+1]) {
+swap(b[i], b[i+1]);
+swap(a[i], a[i+1]);
+swapped = true;
+}
+}
+} while (swapped);
+for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+}
+return 0;
+}
